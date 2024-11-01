4ff9d037542d9f704a08c602e964ac8bacb6a176
Moeed Bhatti <moeedbhatti419@gmail.com>
2024-11-01 17:01:32 +0500

    redirecting page added

--- server/app.js
+++ server/app.js
@@ -16,6 +16,38 @@ app.get("/test", (req, res) => {
   res.status(200).json({ message: "Server Running" });
 });
 
+app.get("/app", (req, res) => {
+  const eventID = req.query.eventID;
+  const role = req.query.role;
+
+  const appLink = `snapfetti://app/${eventID}/${role}`;
+  const appStoreLink =
+    "https://play.google.com/store/apps/details?id=com.snapfetti"; // Android app link
+
+  // Serve HTML with JavaScript for redirection
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Redirecting...</title>
+    </head>
+    <body>
+      <p>Redirecting to the app...</p>
+      <script type="text/javascript">
+        // Attempt to open the app link
+        window.location.href = "${appLink}";
+        
+        // Fallback to the app store after a delay if app link fails
+        setTimeout(() => {
+          window.location.href = "${appStoreLink}";
+        }, 2000);
+      </script>
+    </body>
+    </html>
+  `);
+});
 app.use("*", (req, res, next) => {
   try {
     return res.status(404).json({
