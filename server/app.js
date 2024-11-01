400d2c8db66fb4472c83f87e9b81cce3e42f5840
Moeed Bhatti <moeedbhatti419@gmail.com>
2024-11-01 19:26:43 +0500

    fallback time exceeded

--- server/app.js
+++ server/app.js
@@ -66,7 +66,7 @@ app.get("/app", (req, res) => {
           } else {
             window.location.href = "https://example.com"; // A fallback URL for other devices
           }
-        }, 2000); // Adjust delay as needed
+        }, 10000); // Adjust delay as needed
       </script>
     </body>
     </html>
